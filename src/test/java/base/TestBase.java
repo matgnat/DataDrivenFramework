ff6e95454e39d6fd36a9df616e13bf964cd8ec69
Mateusz Gnat <wir03@MateuszGnat-MacBook-Pro.local>
2018-04-17 07:00:59 +0200

    implement lazy instantiation for driver

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -1,12 +1,57 @@
 package base;
 
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class TestBase {
 
+    public static Properties confing = new Properties();
+    public static Properties locators = new Properties();
+    public static FileInputStream fileInputStream;
+    public static WebDriver driver;
+
+    @BeforeSuite
     public void setUp() {
+
+        if (driver == null) {
+
+            String genericDir = System.getProperty("user.dir");
+
+            try {
+                FileInputStream fileInputStream = new FileInputStream(genericDir + "//src//test//resources//properties//Config.properties");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                confing.load(fileInputStream);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                fileInputStream = new FileInputStream(genericDir + "//src//test//resources//properties//Locators.properties");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                locators.load(fileInputStream);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
 
     }
 
-
+    @AfterSuite
     public void tearDown() {
 
 
---
+++ src/test/java/rought/TestProperties.java
@@ -0,0 +1,26 @@
+package rought;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class TestProperties {
+
+    public static void main(String[] args) throws IOException {
+
+
+        /**
+         * load properties
+         */
+
+        String genericDir = System.getProperty("user.dir");
+
+        FileInputStream fileInputStream = new FileInputStream(genericDir + "//src//test//resources//properties//Config.properties");
+
+        Properties confing = new Properties();
+
+        confing.load(fileInputStream);
+    }
+
+}
